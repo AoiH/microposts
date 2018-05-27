237aa5a4928f822fadfae2d8377e25c1e6a76450
AOI <hatake390o70@yahoo.co.jp>
2018-05-27 05:22:00 +0000

    user registration

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -12,8 +12,8 @@
             </div>
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#">Signup</a></li>
-                    <li><a href="#">Login</a></li>
+                    <li>{!! link_to_route('signup.get', 'Signup')　!!}</li>
+                    <li><a href="#">Login</li>
                 </ul>
             </div>
         </div>
