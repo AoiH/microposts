b476b23b18bd67457109578b430260b7f6be8091
AOI <hatake390o70@yahoo.co.jp>
2018-06-05 07:06:05 +0000

    post

--- app/Http/Controllers/MicropostsController.php
+++ app/Http/Controllers/MicropostsController.php
@@ -16,11 +16,37 @@ class MicropostsController extends Controller
             $data = [
                 'user' => $user,
                 'microposts' => $microposts,
-                ];
-                $date += $this->counts($user);
-                return view('users.show', $data);
+            ];
+            $data += $this->counts($user);
+            return view('users.show', $data);
         }else{
             return view('welcome');
-              }
-           }
-     }
+        }
+    }
+    
+    
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'content' => 'required|max:191',
+        ]);
+
+        $request->user()->microposts()->create([
+            'content' => $request->content,
+        ]);
+
+        return redirect('/');
+    }
+    
+    
+     public function destroy($id)
+    {
+        $micropost = \App\Micropost::find($id);
+
+        if (\Auth::user()->id === $micropost->user_id) {
+            $micropost->delete();
+        }
+
+        return redirect()->back();
+    }
+}
--- resources/views/microposts/microposts.blade.php
+++ resources/views/microposts/microposts.blade.php
@@ -12,9 +12,16 @@
          <div>
             <p>{!! nl2br(e($micropost->content)) !!}</p>
          </div>
-      </div>
-   </li>
+         <div>
+                @if (Auth::user()->id == $micropost->user_id)
+                    {!! Form::open(['route' => ['microposts.destroy', $micropost->id], 'method' => 'delete']) !!}
+                        {!! Form::submit('Delete', ['class' => 'btn btn-danger btn-xs']) !!}
+                    {!! Form::close() !!}
+                @endif
+         </div>
+       </div>
+    </li>
 @endforeach
 </ul>
 {!! $microposts->render() !!}
-
+  
