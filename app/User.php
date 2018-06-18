4024096669dfae99b3e7366818689620238b26f9
AOI <hatake390o70@yahoo.co.jp>
2018-06-18 13:46:10 +0000

    model favorites

--- app/User.php
+++ app/User.php
@@ -84,12 +84,17 @@ class User extends Authenticatable
     return $this->followings()->where('follow_id', $userId)->exists();
     }
     
-      public function feed_microposts()
+    public function feed_microposts()
     {
         $follow_user_ids = $this->followings()-> pluck('users.id')->toArray();
         $follow_user_ids[] = $this->id;
         return Micropost::whereIn('user_id', $follow_user_ids);
     }
+    
+    public function favorites()
+    {
+        return $this->belongsToMany(Micropost::class, 'user_micropost', 'user_id', 'micropost_id')->withTimestamps();
+        }
 
 
         
